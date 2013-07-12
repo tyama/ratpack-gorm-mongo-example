5e3cf8d87abd7a4477e44e683d67a76ad81312bd
T.Yamamoto <mottsnite@gmail.com>
2013-07-12 17:38:55 +0900

    json response.

--- src/ratpack/ratpack.groovy
+++ src/ratpack/ratpack.groovy
@@ -1,6 +1,7 @@
-
 import com.google.inject.AbstractModule
 import static org.ratpackframework.groovy.RatpackScript.ratpack
+
+import com.mongodb.util.JSON
 
 import org.grails.datastore.gorm.mongo.config.*
 import domains.*
@@ -14,15 +15,18 @@ ratpack {
         })
     }
     handlers {
-        get('mongo'){
-            Book.withSession {
+        get('create'){
+            Book.withSession{
                 new Book(title:"The Stand").save(flush:true)
-
-                Book.list().each {
-                    println it.title
-                }
             }
-            response.send "Saved"
+            response.send "created"
+        }
+        get('mongo'){
+            def list
+            Book.withSession {
+                list = Book.collection.find()
+            }
+            response.send "application/json", JSON.serialize(list)
         }
     }
 }
